bcbbe48200f1a922212db21c62834b0bf775c49e
cathrch <ekaterina.zhy@list.ru>
2023-08-04 14:47:34 +0300

    3.2. Загрузка файлов на github [#243562]

---
+++ src/test/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Ekaterina Chernyaeva");
+        System.out.println("07.12.1994");
+
+    }
+}
