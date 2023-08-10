f8a0e7757e7b415d7e028d19fa918d169aac4392
cathrch <ekaterina.zhy@list.ru>
2023-08-10 22:23:21 +0300

    113/2 [#159458]

--- src/main/java/ru/job4j/calculator/Fit.java
+++ src/main/java/ru/job4j/calculator/Fit.java
@@ -2,19 +2,17 @@ package ru.job4j.calculator;
 
 public class Fit {
     public static double maleWeight(double weight) {
-        double ideal = (weight - 100) * 1.15;
-        return ideal;
+        return (weight - 100) * 1.15;
     }
 
     public static double femaleWeight(double weight) {
-        double ideal = (weight - 110) * 1.15;
-        return ideal;
+        return  (weight - 110) * 1.15;
     }
 
     public static void main(String[] args) {
-        short manHeight = 187;
+        short manHeight = 180;
         double male = Fit.maleWeight(manHeight);
-        short womanHeight = 160;
+        short womanHeight = 170;
         double female = Fit.femaleWeight(womanHeight);
         System.out.println("При росте " + manHeight + " идеальный вес мужчины: " + male);
         System.out.println("При росте " + womanHeight + " идеальный вес женщины: " + female);
