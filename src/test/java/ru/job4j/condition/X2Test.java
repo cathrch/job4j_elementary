9a5929a2d8c4c35c0274262932afb207f8b817f1
cathrch <ekaterina.zhy@list.ru>
2023-08-15 21:30:20 +0300

    113/1.2 [#297750]

--- src/test/java/ru/job4j/condition/X2Test.java
+++ src/test/java/ru/job4j/condition/X2Test.java
@@ -6,7 +6,7 @@ import static org.assertj.core.api.Assertions.*;
 class X2Test {
 
     @Test
-    void whenA10B0C0X2Then() {
+    void whenA10B0C0X2Then40() {
         int a = 10;
         int b = 0;
         int c = 0;
@@ -17,7 +17,7 @@ class X2Test {
     }
 
     @Test
-    void whenA1B1C1X1Then() {
+    void whenA1B1C1X1Then3() {
         int a = 1;
         int b = 1;
         int c = 1;
@@ -28,7 +28,7 @@ class X2Test {
     }
 
     @Test
-    void whenA0B1C1X1Then() {
+    void whenA0B1C1X1Then2() {
         int a = 0;
         int b = 1;
         int c = 1;
@@ -39,7 +39,7 @@ class X2Test {
     }
 
     @Test
-    void whenA1B1C0X1Then() {
+    void whenA1B1C0X1Then2() {
         int a = 1;
         int b = 1;
         int c = 0;
@@ -50,7 +50,7 @@ class X2Test {
     }
 
     @Test
-    void whenA1B1C1X0Then() {
+    void whenA1B1C1X0Then1() {
         int a = 1;
         int b = 1;
         int c = 1;
