6a93e495ebd34086d2b3d8a9b426f754099b09f9
cathrch <ekaterina.zhy@list.ru>
2023-09-12 00:45:45 +0300

    115/5.2 [193]

---
+++ src/test/java/ru/job4j/loop/FactorialTest.java
@@ -0,0 +1,32 @@
+package ru.job4j.loop;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class FactorialTest {
+
+    @Test
+    void whenCalcFactorialForFiveThenOneHundredTwenty() {
+    int expected = 120;
+    int n = 5;
+    int out = Factorial.calc(n);
+    assertThat(out).isEqualTo(expected);
+    }
+
+    @Test
+    void whenCalcFactorialForZeroThenOne() {
+        int expected = 1;
+        int n = 0;
+        int out = Factorial.calc(n);
+        assertThat(out).isEqualTo(expected);
+    }
+
+    @Test
+    void whenCalcFactorialForOneThenOne() {
+        int expected = 1;
+        int n = 1;
+        int out = Factorial.calc(n);
+        assertThat(out).isEqualTo(expected);
+    }
+}
