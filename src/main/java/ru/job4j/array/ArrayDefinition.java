29eb108db9993aeb88950e612f15e7e29115c73e
cathrch <ekaterina.zhy@list.ru>
2023-09-16 22:05:44 +0300

    116/6.0.3 [226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,14 @@ public class ArrayDefinition {
         System.out.println("Array size: " + surnames.length);
         float[] prices = new float[40];
         System.out.println("Array size: " + prices.length);
+        String[] names = new String[4];
+        names[0] = "Petr";
+        names[1] =  "Ekaterina";
+        names[2] = "Anna";
+        names[3] = "Ivan";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
